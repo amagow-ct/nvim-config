c6b873afd8df6a050291f9db72d1e830f05236a6
Adwithya Magow <a.magow@chaostheory.hk>
2023-02-23 15:39:59 +0800

    Remove old module

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,19 +3,6 @@ require('nvim-treesitter.configs').setup({
     enable = true,
     additional_vim_regex_highlighting = false,
   },
-  -- :help nvim-treesitter-textobjects-modules
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      }
-    },
-  },
   ensure_installed = {
     'javascript',
     'typescript',
