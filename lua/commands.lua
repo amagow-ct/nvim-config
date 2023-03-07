36b0ed947ff3010e63e0281bf61799d2a80499a0
Adwithya Magow <a.magow@chaostheory.hk>
2023-03-07 12:06:13 +0800

    Fix fmt

--- lua/commands.lua
+++ lua/commands.lua
@@ -4,7 +4,7 @@ local api = vim.api
 vim.cmd('colorscheme onedark')
 
 -- Does not work unless is a callback
-local foldCallback = function() vim.schedule(function() vim.cmd("normal zR") end) end 
+local foldCallback = function() vim.schedule(function() vim.cmd("normal zR") end) end
 local treesitterFoldGroup = api.nvim_create_augroup("TreesitterOpenFolds", { clear = true })
 api.nvim_create_autocmd({"BufReadPost", "FileReadPost"}, {
   pattern = "*",
