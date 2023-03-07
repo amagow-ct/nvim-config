c55da19d63c5ad1055ceb0b95e96cd2a77b17051
Adwithya Magow <a.magow@chaostheory.hk>
2023-03-07 12:06:20 +0800

    Add sql for mason

--- lua/lsp/servers.lua
+++ lua/lsp/servers.lua
@@ -1,6 +1,6 @@
 
 local M = {}
 
-M.server_list = {"bashls", "gopls", "lua_ls", "dockerls", "docker_compose_language_service", "bufls"}
+M.server_list = {"bashls", "gopls", "lua_ls", "dockerls", "docker_compose_language_service", "bufls", "sqlls"}
 
 return M
