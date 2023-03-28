3b46e1ddf60114fd20501e15b37a1fe02a6e482c
Adwithya Magow <a.magow@chaostheory.hk>
2023-03-28 18:45:31 +0800

    Add rust rust_analyzer to server list

--- lua/lsp/servers.lua
+++ lua/lsp/servers.lua
@@ -1,6 +1,6 @@
-
 local M = {}
 
-M.server_list = {"bashls", "gopls", "lua_ls", "dockerls", "docker_compose_language_service", "bufls", "sqlls"}
+M.server_list = { "bashls", "gopls", "lua_ls", "dockerls", "docker_compose_language_service", "bufls", "sqlls",
+  "rust_analyzer" }
 
 return M
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,6 @@
 local serverList = require("lsp.servers").server_list
 require('mason').setup()
-require("mason-lspconfig").setup { 
+require("mason-lspconfig").setup {
   ensure_installed = serverList,
   automatic_installation = true,
 }
-
